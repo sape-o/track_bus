1f4edd8510b2bd2e3eea8a547a487fd094441e7e
oreo <sqlmario@hotmail.com>
2018-09-30 16:12:25 +0700

    addNewFile

---
+++ user-app/mobileapp/src/App.js
@@ -0,0 +1,20 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var ons = require('onsenui');
+var Ons = require('react-onsenui');
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
