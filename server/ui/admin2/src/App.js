0c48c7f1a4aca8579e408cddca51542528a0c2ef
oreo <sqlmario@hotmail.com>
2018-12-16 03:14:50 +0700

    make login non finish

--- server/ui/admin2/src/App.js
+++ server/ui/admin2/src/App.js
@@ -1,26 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {InputText} from 'primereact/inputtext';
+import {Password} from 'primereact/password';
+import {Button} from 'primereact/button';
+import {Card} from 'primereact/card';
+
+import 'primereact/resources/themes/nova-light/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+
+  constructor() {
+      super();
+      this.state={
+        username:null,
+        password:null,
+
+
+      }
+  }
+
+  render(){
+    return(
+
+      <Card style={{width:'300px'}}  >
+        <span className="p-float-label">
+          <InputText id="in" value={this.state.username} onChange={(e) => this.setState({username: e.target.value})} />
+          <label htmlFor="in">Username</label>
+        </span>
+        <br/>
+        <span className="p-float-label">
+          <Password id="in" value={this.state.password} onChange={(e) => this.setState({password: e.target.value})} />
+          <label htmlFor="in">Password</label>
+        </span>
+        <br/>
+        <div>
+          <Button label="Sing In" /> &nbsp;
+          <Button label="Sing Up" className="p-button-success"/>
+        </div>
+      </Card>
+
+
     );
   }
 }
