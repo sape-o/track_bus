44c019b149e68b32c5c6a097ddb210115a66b44e
oreo <sqlmario@hotmail.com>
2018-12-16 07:56:32 +0700

    change name.js to index.js

--- server/ui/admin2/src/App.js
+++ server/ui/admin2/src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import {Route,Switch,Link} from 'react-router-dom';
-
-import {Signin} from './signin/signin';
-import {Home} from './home/home';
-import {Bus} from './bus/bus';
+import {Document} from './document';
+import {Signin} from './signin/';
+import {Home} from './home/';
+import {Bus} from './bus/';
 import 'primereact/resources/themes/nova-light/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
@@ -18,30 +18,36 @@ export default class App extends Component {
 
 
   render(){
-    return(
-      <div>
-      <nav class="navbar is-primary" role="navigation" aria-label="main navigation" style={{width:'100%'}}>
-        <div id="navbarBasicExample" class="navbar-menu">
-          <div class="navbar-start">
-              <Link to="/" class="navbar-item">SUT Track BUS</Link>
-            <a class="navbar-item">Home</a>
-            <a class="navbar-item">Documentation</a>
-          </div>
-          <div class="navbar-end">
-            <div class="navbar-item">
-              <div class="buttons">
-             <Link to="/signin" class="button is-success">Sign Up</Link>
-             <Link to="/Signup" class="button is-success">Sign In</Link>
+    if(true){
+      return(
+        <div>
+          <nav class="navbar is-primary" role="navigation" aria-label="main navigation" style={{width:'100%'}}>
+            <div id="navbarBasicExample" class="navbar-menu">
+              <div class="navbar-start">
+                <Link to="/" class="navbar-item">SUT Track BUS</Link>
+                <Link to="/home" class="navbar-item">Home</Link>
+                <Link to="/document" class="navbar-item">Documentation</Link>
+              </div>
+              <div class="navbar-end">
+                <div class="navbar-item">
+                  <div class="buttons">
+                 <Link to="/signin" class="button is-success">Sign Up</Link>
+                 <Link to="/signup" class="button is-success">Sign In</Link>
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-        </div>
-      </nav>
+          </nav>
 
-      <React.Fragment>
-        <Route path="/signin" component={Signin} />
-      </React.Fragment>
-    </div>
-  );
+          <React.Fragment>
+            <Route path="/home" component={Home} />
+            <Route path="/document" component={Document} />
+            <Route path="/signin" component={Signin} />
+
+
+          </React.Fragment>
+        </div>
+      );
+    }
   }
 }
---
+++ server/ui/admin2/src/document/index.js
@@ -0,0 +1,11 @@
+import React,{Component} from 'react';
+
+export class Document extends Component{
+
+render(){
+  return(
+    <h1>Document</h1>
+  );
+}
+
+}
